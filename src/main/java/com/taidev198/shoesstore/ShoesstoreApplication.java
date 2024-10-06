82fce5d62faf7df94d0a32cd670cae837a7506ad
traigiuxom9x@gmail.com <traigiuxom9x@gmail.com>
2024-10-06 21:20:55 +0700

    Initial commit

---
+++ src/main/java/com/taidev198/shoesstore/ShoesstoreApplication.java
@@ -0,0 +1,13 @@
+package com.taidev198.shoesstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taidev198/shoesstore/ShoesstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taidev198.shoesstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
